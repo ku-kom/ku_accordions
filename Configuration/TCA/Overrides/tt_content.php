68ad4ce5a6adab408c2bbafb51ecbc121ef9cb09
nanna-dk <nanna@e-nanna.dk>
2023-02-02 15:04:55 +0100

    Setup

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -10,38 +10,40 @@
 
  defined('TYPO3') or die('Access denied.');
 
+$extension = 'ku_accordions';
+
  // Add Content Element
- if (!is_array($GLOBALS['TCA']['tt_content']['types']['ku-accordions'] ?? false)) {
-     $GLOBALS['TCA']['tt_content']['types']['ku-accordions'] = [];
+ if (!is_array($GLOBALS['TCA']['tt_content']['types'][$extension] ?? false)) {
+     $GLOBALS['TCA']['tt_content']['types'][$extension] = [];
  }
 
-// KU accordions
+// KU accordions container
 \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->configureContainer(
     (
         new \B13\Container\Tca\ContainerConfiguration(
-            'ku_accordions', // CType
+            $extension, // CType
             'LLL:EXT:ku_accordions/Resources/Private/Language/locallang_be.xlf:label', // label
             'LLL:EXT:ku_accordions/Resources/Private/Language/locallang_be.xlf:description', // description
             [
                 [
                     ['name' => 'LLL:EXT:ku_accordions/Resources/Private/Language/locallang_be.xlf:add_content', 'colPos' => 203, 'allowed' => ['CType' => 'text, textmedia, textpic, image']]
                 ]
-            ] // grid configuration
+            ]
         )
     )
     ->setIcon('EXT:ku_accordions/Resources/Public/Icons/Extension.svg')
 );
 
 // Configure element type
-$GLOBALS['TCA']['tt_content']['types']['ku_accordions'] = array_replace_recursive(
-    $GLOBALS['TCA']['tt_content']['types']['ku_accordions'],
+$GLOBALS['TCA']['tt_content']['types'][$extension] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types'][$extension],
     [
         'showitem' => '
             --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
             --div--;LLL:EXT:ku_accordions/Resources/Private/Language/locallang_be.xlf:options,
-                pi_flexform;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:advanced,
+                pi_flexform;LLL:EXT:ku_accordions/Resources/Private/Language/locallang_be.xlf:advanced,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
@@ -63,5 +65,5 @@ $GLOBALS['TCA']['tt_content']['types']['ku_accordions'] = array_replace_recursiv
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     '*',
     'FILE:EXT:ku_accordions/Configuration/FlexForms/KuAccordions.xml',
-    'ku_accordions' // CType
+    $extension
 );
