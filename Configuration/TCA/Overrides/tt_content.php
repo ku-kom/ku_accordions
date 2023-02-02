99d7753968974f4f6f184830f9869aea7e11b555
nanna-dk <nanna@e-nanna.dk>
2023-02-02 11:39:17 +0100

    Init

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+// KU accordions
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->configureContainer(
+    (
+        new \B13\Container\Tca\ContainerConfiguration(
+            'ku-accordions', // CType
+            'KU accordions', // label
+            'Create accordions with various content', // description
+            [
+                [
+                    ['name' => 'Add accordion content', 'colPos' => 203, 'allowed' => ['CType' => 'text, textmedia, textpic, image']]
+                ]
+            ] // grid configuration
+        )
+    )
+    ->setIcon('EXT:ku_accordions/Resources/Public/Icons/Accordion.svg')
+);
+
+// Configure element type
+$GLOBALS['TCA']['tt_content']['types']['ku-accordions'] = array_replace_recursive(
+    $GLOBALS['TCA']['tt_content']['types']['ku-accordions'],
+    [
+        'showitem' => '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
+            --div--;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:accordion.options,
+                pi_flexform;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:advanced,
+            --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                --palette--;;language,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                --palette--;;hidden,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                categories,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                rowDescription,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+        '
+    ]
+);
+
+// Add flexForms for content element configuration
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    '*',
+    'FILE:EXT:ku_accordions/Configuration/FlexForms/KuAccordions.xml',
+    'ku-accordions' // CType
+);
